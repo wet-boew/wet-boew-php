4b0f2d876440a93bbc29a5ee723d6b466edb9611
upsonp <upsonp@gmail.com>
2013-04-17 14:54:20 -0300

    Update header-entete.php
    Fixed the PHP header to use the svg images and fall back to the png images. It seems to work, I spent half an hour trying to get the svg image to show up when I realized the server has to have the svg mimetype added to it for the svg image to work.

--- demos-php/theme-gcwu-fegc/inc/cont/header-entete.php
+++ demos-php/theme-gcwu-fegc/inc/cont/header-entete.php
@@ -1,6 +1,6 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/demos-php/inc/body-corps-1a.php"; ?>
 <nav role="navigation"><div id="gcwu-gcnb"><h2><?php echo $_UNKNOWN['gc_gcnavbar_' . $_PAGE['lang1']]; ?></h2><div id="gcwu-gcnb-in"><div id="gcwu-gcnb-fip">
-<div id="gcwu-sig"><div id="gcwu-sig-in"><div id="gcwu-sig-<?php echo $_PAGE['lang1']; ?>" title="<?php echo $_UNKNOWN['gc_sig_' . $_PAGE['lang1']]; ?>"><img src="/build/theme-gcwu-fegc/images/<?php echo $_UNKNOWN['gc_sig_image']; ?>-<?php echo $_PAGE['lang1']; ?>.gif" width="214" height="20" alt="<?php echo $_UNKNOWN['gc_sig_' . $_PAGE['lang1']]; ?>" /></div></div></div>
+<div id="gcwu-sig"><div id="gcwu-sig-in"><object data="/build/theme-gcwu-fegc/images/<?php echo $_UNKNOWN['gc_sig_image']; ?>-<?php echo $_PAGE['lang1']; ?>.svg" role="img" tabindex="-1" aria-label="<?php echo $_UNKNOWN['gc_sig_' . $_PAGE['lang1']]; ?>" type="image/svg+xml"><img src="/build/theme-gcwu-fegc/images/<?php echo $_UNKNOWN['gc_sig_image']; ?>-<?php echo $_PAGE['lang1']; ?>.png" alt="<?php echo $_UNKNOWN['gc_sig_' . $_PAGE['lang1']]; ?>" /></object></div></div>
 <ul>
 <li id="gcwu-gcnb1"><a<?php if ($_PAGE['isapp'] == '1') { ?> target="_blank"<?php } ?> rel="external" href="<?php echo $_UNKNOWN['gc_gcnb1_href_' . $_PAGE['lang1']]; ?>"><?php echo $_UNKNOWN['gc_gcnb1_text_' . $_PAGE['lang1']]; ?></a></li>
 <li id="gcwu-gcnb2"><a<?php if ($_PAGE['isapp'] == '1') { ?> target="_blank"<?php } ?> rel="external" href="<?php echo $_UNKNOWN['gc_gcnb2_href_' . $_PAGE['lang1']]; ?>"><?php echo $_UNKNOWN['gc_gcnb2_text_' . $_PAGE['lang1']]; ?></a></li>
@@ -10,7 +10,7 @@
 </div></div></div></nav>
 
 <div id="gcwu-bnr" role="banner"><div id="gcwu-bnr-in">
-<div id="gcwu-wmms"><div id="gcwu-wmms-in"><div id="gcwu-wmms-fip" title="<?php echo $_UNKNOWN['gc_wmms_' . $_PAGE['lang1']]; ?>"><img src="/build/theme-gcwu-fegc/images/wmms.gif" width="126" height="30"  alt="<?php echo $_UNKNOWN['gc_wmms_' . $_PAGE['lang1']]; ?>" /></div></div></div>
+<div id="gcwu-wmms"><div id="gcwu-wmms-in"><object data="/build/theme-gcwu-fegc/images/wmms.svg" role="img" tabindex="-1" aria-label="<?php echo $_UNKNOWN['gc_wmms_' . $_PAGE['lang1']]; ?>" type="image/svg+xml"><img src="/build/theme-gcwu-fegc/images/wmms.png" alt="<?php echo $_UNKNOWN['gc_wmms_' . $_PAGE['lang1']]; ?>" /></object></div></div>
 
 <div id="gcwu-title"><p id="gcwu-title-in"><a href="<?php echo $_UNKNOWN['gc_banner_href_' . $_PAGE['lang1']]; ?>"><?php echo $_UNKNOWN['gc_site_title_' . $_PAGE['lang1']]; ?></a></p></div>
 
