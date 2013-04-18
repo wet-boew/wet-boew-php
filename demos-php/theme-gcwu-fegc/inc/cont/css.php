53bf206c659dafc1433848cc891b6654182b922b
upsonp <upsonp@gmail.com>
2013-04-18 13:58:13 -0300

    Update css.php
    Changed path from /build to /dist

--- demos-php/theme-gcwu-fegc/inc/cont/css.php
+++ demos-php/theme-gcwu-fegc/inc/cont/css.php
@@ -3,21 +3,21 @@
 <link rel="shortcut icon" href="/dist/theme-gcwu-fegc/images/favicon.ico" />
 
 <!--[if lte IE 8]>
-<script src="/build/js/jquery-ie.min.js"></script>
-<script src="/build/js/polyfills/html5shiv-min.js"></script>
-<link rel="stylesheet" href="/build/grids/css/util-ie-min.css" />
-<link rel="stylesheet" href="/build/js/css/pe-ap-ie-min.css" />
-<link rel="stylesheet" href="/build/theme-gcwu-fegc/css/theme-ie-min.css" />
-<noscript><link rel="stylesheet" href="/build/theme-gcwu-fegc/css/theme-ns-ie-min.css" /></noscript>
+<script src="/dist/js/jquery-ie.min.js"></script>
+<script src="/dist/js/polyfills/html5shiv-min.js"></script>
+<link rel="stylesheet" href="/dist/grids/css/util-ie-min.css" />
+<link rel="stylesheet" href="/dist/js/css/pe-ap-ie-min.css" />
+<link rel="stylesheet" href="/dist/theme-gcwu-fegc/css/theme-ie-min.css" />
+<noscript><link rel="stylesheet" href="/dist/theme-gcwu-fegc/css/theme-ns-ie-min.css" /></noscript>
 <![endif]-->
 <!--[if gt IE 8]><!-->
 <script src="/build/js/jquery.min.js"></script>
-<link rel="stylesheet" href="/build/grids/css/util-min.css" />
-<link rel="stylesheet" href="/build/js/css/pe-ap-min.css" />
-<link rel="stylesheet" href="/build/theme-gcwu-fegc/css/theme-min.css" />
-<noscript><link rel="stylesheet" href="/build/theme-gcwu-fegc/css/theme-ns-min.css" /></noscript>
+<link rel="stylesheet" href="/dist/grids/css/util-min.css" />
+<link rel="stylesheet" href="/dist/js/css/pe-ap-min.css" />
+<link rel="stylesheet" href="/dist/theme-gcwu-fegc/css/theme-min.css" />
+<noscript><link rel="stylesheet" href="/dist/theme-gcwu-fegc/css/theme-ns-min.css" /></noscript>
 <!--<![endif]-->
-<noscript><link rel="stylesheet" href="/build/theme-gcwu-fegc/css/theme-ns-min.css" /></noscript>
+<noscript><link rel="stylesheet" href="/dist/theme-gcwu-fegc/css/theme-ns-min.css" /></noscript>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/demos-php/inc/head-tete-2.php"; ?>
 
 <!-- CustomCSSStart -->
