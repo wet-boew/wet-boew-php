e7df0d89150703f3aab6f0031351e00664917eb6
upsonp <upsonp@gmail.com>
2013-04-18 10:35:43 -0300

    Update body-corps-2b.php
    Fixed issues with the pathing pointing to ../dist and using theme-wet-boew instead of theme-gcwu-boew

--- demos-php/inc/body-corps-2b.php
+++ demos-php/inc/body-corps-2b.php
@@ -5,14 +5,14 @@
 <!-- ScriptsStart -->
 <script src="../dist/js/settings.js"></script>
 <!--[if lte IE 8]>
-<script src="../dist/theme-wet-boew/js/theme-ie-min.js"></script>
-<script src="../dist/js/pe-ap-ie-min.js"></script>
-<script src="../dist/js/jquerymobile/jquery.mobile-ie.min.js"></script>
+<script src="/build/theme-gcwu-fegc/js/theme-ie-min.js"></script>
+<script src="/build/js/pe-ap-ie-min.js"></script>
+<script src="/build/js/jquerymobile/jquery.mobile-ie.min.js"></script>
 <![endif]-->
 <!--[if gt IE 8]><!-->
-<script src="../dist/theme-wet-boew/js/theme-min.js"></script>
-<script src="../dist/js/pe-ap-min.js"></script>
-<script src="../dist/js/jquerymobile/jquery.mobile.min.js"></script>
+<script src="/build/theme-gcwu-fegc/js/theme-min.js"></script>
+<script src="/build/js/pe-ap-min.js"></script>
+<script src="/build/js/jquerymobile/jquery.mobile.min.js"></script>
 <!--<![endif]-->
 <!-- ScriptsEnd -->
 
