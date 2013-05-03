8e0baa7461ac903c00de1f3f031899f1c4a34448
upsonp <upsonp@gmail.com>
2013-05-03 11:23:17 -0300

    Added theme demos need a break

---
+++ demos-php/config/theme-gcwu-intranet/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$_ROOT_SERVER_DIR = "/wet-boew";
+	//$_ROOT_SERVER_DIR .= "-php";
+
+		//override variables set in the dist config folder here to personalize the site
+	$_DIST_CONFIG_LOC_ = $_ROOT_SERVER_DIR . "/dist-php/config/theme-base/config.php";
+	include $_SERVER['DOCUMENT_ROOT'] . $_DIST_CONFIG_LOC_;
+		
+	$_SITE['wb_site_href_eng'] = $_ROOT_SERVER_DIR . "/demos-php/index.php"; 
+	$_SITE['wb_site_href_fra'] = $_ROOT_SERVER_DIR . "/demos-php/index.php"; 
+
+	/*	Override variables in the dist-php config files here for the specific site */
+
+?>
