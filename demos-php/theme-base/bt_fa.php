42218153e195ddbda98ca0fc2dcadad301693e37
upsonp <upsonp@gmail.com>
2013-05-03 10:35:04 -0300

    Renamed $_NAV_PATH to $_NAVPATH to be more consisitant with previons WET versions

--- demos-php/theme-base/bt_fa.php
+++ demos-php/theme-base/bt_fa.php
@@ -9,9 +9,9 @@ if( strpos($_ROOT_PATH_, "\\") > 0 ) {
 //Remove the server root path to make the path a nice URL relaitve path
 $_PATH_ = ((strtoupper(substr(PHP_OS, 0, 3)) == 'WIN')?"":"/") .preg_replace("(".$_ROOT_PATH_.")", "", realpath(dirname(__FILE__)));
 
-$_NAV_PATH['title_eng'] = "Base Theme";
-$_NAV_PATH['link_eng'] = $_PATH_ ."/index-eng.php";
+$_NAVPATH['title_eng'] = "Base Theme";
+$_NAVPATH['link_eng'] = $_PATH_ ."/index-eng.php";
 
-$_NAV_PATH['title_fra'] = "Thème de base";
-$_NAV_PATH['link_fra'] = $_PATH_ ."/index-fra.php";
+$_NAVPATH['title_fra'] = "Thème de base";
+$_NAVPATH['link_fra'] = $_PATH_ ."/index-fra.php";
 ?>
--- dist-php/inc/bt_fa.php
+++ dist-php/inc/bt_fa.php
@@ -1,14 +1,14 @@
 <?php 
-$_NAV_PATH['parts'] = explode("/", urldecode($_SERVER['REQUEST_URI']));
-array_pop($_NAV_PATH['parts']);
-$_NAV_PATH_BASE = $_SERVER['DOCUMENT_ROOT'];
-foreach($_NAV_PATH['parts'] as $_NAV_PATH_PART) {
-	$_NAV_PATH_BASE .= $_NAV_PATH_PART . "/";
-	if (file_exists($_NAV_PATH_BASE . "bt_fa.php")) {
-		include($_NAV_PATH_BASE . "bt_fa.php");
-		$title = $_NAV_PATH['title_' . $_PAGE['lang1']];
-		if( $_NAV_PATH['link_'. $_PAGE['lang1']] ) {
-			$title = "<a href=\"" . $_NAV_PATH['link_'.$_PAGE['lang1']] ."\">".$title."</a>";
+$_NAVPATH['parts'] = explode("/", urldecode($_SERVER['REQUEST_URI']));
+array_pop($_NAVPATH['parts']);
+$_NAVPATH_BASE = $_SERVER['DOCUMENT_ROOT'];
+foreach($_NAVPATH['parts'] as $_NAVPATH_PART) {
+	$_NAVPATH_BASE .= $_NAVPATH_PART . "/";
+	if (file_exists($_NAVPATH_BASE . "bt_fa.php")) {
+		include($_NAVPATH_BASE . "bt_fa.php");
+		$title = $_NAVPATH['title_' . $_PAGE['lang1']];
+		if( $_NAVPATH['link_'. $_PAGE['lang1']] ) {
+			$title = "<a href=\"" . $_NAVPATH['link_'.$_PAGE['lang1']] ."\">".$title."</a>";
 		}
 		echo "<li>" .$title ."</li>";
 	}
