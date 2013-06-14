d20326774c59ac4afc9f98a3144c9cd6575f50ab
Patrick Upson <upsonp@gmail.com>
2013-06-14 08:13:57 -0300

    Update bt_fa.php
    Removed dynamic pathing from BC trail for Azure site

--- demos-php/theme-base/bt_fa.php
+++ demos-php/theme-base/bt_fa.php
@@ -1,22 +1,7 @@
 <?php
-
-//get the path to the current folder this file resides in. 
-$_ROOT_PATH_ = $_SERVER['DOCUMENT_ROOT'];
-
-$file_path = realpath(dirname(__FILE__));
-
-//realpath on windows will return the path using backslashes
-$file_path = preg_replace("(\\\\)", "/", $file_path);
-
-//remove the document root from the path to this file
-$file_path = preg_replace("(".$_ROOT_PATH_.")", "", $file_path);
-
-//on linux and Unix servers the forward slash needs to be added
-$_PATH_ = ((strtoupper(substr(PHP_OS, 0, 3)) == 'WIN')?"":"/") .$file_path;
-
 $_NAVPATH['title_eng'] = "Base Theme";
-$_NAVPATH['link_eng'] = $_PATH_ ."/index-eng.php";
+$_NAVPATH['link_eng'] = "/demos-php/theme-base/index-eng.php";
 
 $_NAVPATH['title_fra'] = "Thème de base";
-$_NAVPATH['link_fra'] = $_PATH_ ."/index-fra.php";
+$_NAVPATH['link_fra'] = "/demos-php/theme-base/index-fra.php";
 ?>
