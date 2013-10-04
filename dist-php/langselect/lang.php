246d4a1d65631ff71761dd17b921e5503b493d98
upsonp <upsonp@gmail.com>
2013-10-04 15:41:11 -0300

    update to lang file

--- dist-php/langselect/lang.php
+++ dist-php/langselect/lang.php
@@ -1,22 +1,26 @@
 <?php
 /*!
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
- * wet-boew.github.io/wet-boew/License-eng.html / wet-boew.github.io/wet-boew/Licence-fra.html
+ * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  */
 if ($_SERVER['HTTP_REFERER'] != "") {
-	$q = $_SERVER['HTTP_REFERER'];
+    $q = $_SERVER['HTTP_REFERER'];
 } else {
-	$q = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['QUERY_STRING'];
+    $q = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['QUERY_STRING'];
 }
 $q = preg_replace('/#(.*)$/', '', $q);
 if (preg_match('/_e\./', $q) > 0) {
-	$q = preg_replace('/_e\./', '_f.', $q);
+    $q = preg_replace('/_e\./', '_f.', $q);
+} else if (preg_match('/-en\./', $q) > 0) {
+    $q = preg_replace('/-en\./', '-fr.', $q);
 } else if (preg_match('/-eng\./', $q) > 0) {
-	$q = preg_replace('/-eng\./', '-fra.', $q);
+    $q = preg_replace('/-eng\./', '-fra.', $q); 
 } else if (preg_match('/_f\./', $q) > 0) {
-	$q = preg_replace('/_f\./', '_e.', $q);
+    $q = preg_replace('/_f\./', '_e.', $q);
+} else if (preg_match('/-fr\./', $q) > 0) {
+    $q = preg_replace('/-fr\./', '-en.', $q);
 } else if (preg_match('/-fra\./', $q) > 0) {
-	$q = preg_replace('/-fra\./', '-eng.', $q);
+    $q = preg_replace('/-fra\./', '-eng.', $q); 
 }
 header("Location: " . $q . "\n\n");
 exit();
