451105b3fe0b368367191d582a9a29f63663f255
upsonp <upsonp@gmail.com>
2014-01-13 20:07:28 -0400

    fixed demos and left side menu

--- demos-php/menu-left-gauche/secnav1-en.php
+++ demos-php/menu-left-gauche/secnav1-en.php
@@ -1,7 +1,11 @@
 <ul class="list-group menu list-unstyled">
 <li><a class="list-group-item item" href="#sect1">Section 1</a>
 <ul class="list-group list-unstyled" id="sect1">
-<li><a class="list-group-item" href="/v4.0-ci/index-en">Item 1a</a></li>
+<li><a class="list-group-item" href="/v4.0-ci/index-en">Item 1a</a>
+	<ul><li><a class="list-group-item" href="#">Item 1a.1</a></li>
+		<ul><li><a href="#">Item 1a.1.a</a></li></ul>
+	</ul>
+</li>
 <li><a class="list-group-item" href="/v4.0-ci/index-en">Item 1b</a></li>
 <li><a class="list-group-item" href="/v4.0-ci/index-en">Item 1c</a></li>
 <li><a class="list-group-item" href="/v4.0-ci/index-en">Item 1d</a></li>
