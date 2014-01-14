1dce734d86b572787e8276616ce732f39f6d0748
upsonp <upsonp@gmail.com>
2014-01-13 20:10:57 -0400

    added sample content files, config and search

---
+++ demos-php/search/search.php
@@ -0,0 +1,7 @@
+<form action="#" method="post" role="search" class="form-inline">
+<div class="form-group">
+<label for="wb-srch-q"><?php echo $_SITE['wb_search_label_' . $_PAGE['lang1']]; ?></label>
+<input id="wb-srch-q" class="form-control" name="wb-srch-q" type="search" value="" size="27" maxlength="150"/>
+</div>
+<button type="submit" id="wb-srch-sub" class="btn btn-default" name="wb-srch-sub"><?php echo $_SITE['wb_search_' . $_PAGE['lang1']]; ?></button>
+</form>
