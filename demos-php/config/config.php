31e69774d7d960ddf964f0ac2d424ae167265b5d
upsonp <upsonp@gmail.com>
2014-02-16 13:07:04 -0400

    Added GCWU theme head-css.php, inc/head.php

--- demos-php/config/config.php
+++ demos-php/config/config.php
@@ -1,4 +1,8 @@
 <?php
+	/*
+	 * Chose a theme
+	 */ 
+	 $_SITE['wb_theme'] = "theme-gcwu-fegc";
 
 	//$_SITE['wb_php_root'] and $_SITE['wb_core_root'] are set in the following file
 	/* **************************************
@@ -6,9 +10,19 @@
 	 * simplify deployment to various webservers. Change the $_SITE['wb_php_root']
 	 * and $_SITE['wb_core_root'] variables to point to the directory where
 	 * the /dist-php and core /dist folders respectivly exist
+	 * 
+	 * DEV NOTE:
+	 * my personal setup has the WET core themes at the root of the server so:
+	 * /demos-php
+	 * /dist-php
+	 * /theme-base
+	 * /theme-gcwu-fegc
+	 * etc...
+	 * 
+	 * I've give the PHP distribution themes the same names as the core theme folders
 	 */
-	 $_SITE['wb_php_root'] = "/wet-boew-php";
-	 $_SITE['wb_core_root'] = "/wet-boew"; 
+	 $_SITE['wb_php_root'] = "";
+	 $_SITE['wb_core_root'] = "/".$_SITE['wb_theme'];
 	
 	//include the standard distribution config with the default settings
 	include $_SERVER['DOCUMENT_ROOT'] . $_SITE['wb_php_root'] . "/dist-php/config/config.php";
