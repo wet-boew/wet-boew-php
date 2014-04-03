a8ee17906cd0295cd72b24319cb75912c2c49edf
Patrick Upson <upsonp@gmail.com>
2014-04-03 09:19:50 -0300

    Updated menu examples

---
+++ demos-php/theme-gcwu-fegc/menu-left-gauche/secnav1-en.php
@@ -0,0 +1,15 @@
+<ul class="list-group menu list-unstyled">
+<li>
+<h3><a class="wb-navcurr">Content page - Secondary menu</a></h3>
+<ul class="list-group menu list-unstyled">
+<li><a class="list-group-item" href="/index-en">Item&#32;1</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;2</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;3</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;4</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;5</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;6</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;7</a></li>
+<li><a class="list-group-item" href="/index-en">Item&#32;8</a></li>
+</ul>
+</li>
+</ul>
---
+++ demos-php/theme-gcwu-fegc/menu-left-gauche/secnav1-fr.php
@@ -0,0 +1,15 @@
+<ul class="list-group menu list-unstyled">
+<li>
+<h3><a class="wb-navcurr">Page de contenu - Menu Secondaire</a></h3>
+<ul class="list-group menu list-unstyled">
+<li><a class="list-group-item" href="/index-fr">Article&#32;1</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;2</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;3</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;4</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;5</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;6</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;7</a></li>
+<li><a class="list-group-item" href="/index-fr">Article&#32;8</a></li>
+</ul>
+</li>
+</ul>
