21d32919df2a9d354ddfdd008b7092e9a132c338
Patrick Upson <upsonp@gmail.com>
2014-09-11 11:04:45 -0300

    update to version number of confing.php file

--- dist-php/config/config.php
+++ dist-php/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$_SITE['wb_version'] = '4.0.1';
+	$_SITE['wb_version'] = '3.1';
 	/*
 	 * Add all of the default variables that may be null to prevent PHP warnings 
 	 * and errors for undefined values
