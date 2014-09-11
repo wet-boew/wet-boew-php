a255961365e359b8317d773a6b3904e001d5f076
Patrick Upson <upsonp@gmail.com>
2014-09-11 15:34:13 -0300

    Having trouble merging from origin/v4.0 to local

--- dist-php/theme-gcwu-fegc/cont/head.php
+++ dist-php/theme-gcwu-fegc/cont/head.php
@@ -6,38 +6,46 @@ $_SIG_ALT_ = $_SITE['gcwu_sig_' . $_PAGE['lang1']];
 $_WMMS_IMG_ = $_SITE['gcwu_wmms_image_'.$_PAGE['lang1']];
 $_WMMS_ALT_ = $_SITE['gcwu_wmms_' . $_PAGE['lang1'] ];
 ?>
-	<div class="container">
-		<div class="row">
-			<object id="gcwu-sig" type="image/svg+xml" tabindex="-1" role="img" data="<?php echo $_SIG_IMG_; ?>" aria-label="<?php echo $_SIG_ALT_; ?>"></object>
-			<ul id="gc-bar" class="list-inline">
-				<li><a href="<?php echo $_SITE['gcwu_head_canada_dot_ca_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_canada_dot_ca_'.$_PAGE['lang1']]; ?></a></li>
-				<li><a href="<?php echo $_SITE['gcwu_head_service_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_service_'.$_PAGE['lang1']]; ?></a></li>
-				<li><a href="<?php echo $_SITE['gcwu_head_departments_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_departments_'.$_PAGE['lang1']]; ?></a></li>
-				<li id="wb-lng"><h2><?php echo $_SITE['wb_lang_header_'.$_PAGE['lang1']]; ?></h2>
-				<ul class="list-inline">
-				 <?php
-	                if( $_PAGE['nolang'] != "1" ) {
-	                    $q= substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], $_PAGE['lang1']."."));
-	                    $langString = "";
-	                    for($i=2; isset($_PAGE['lang'.$i]); $i++ ) {
-	                        $listItem = '<li>';
-	                        $listItem .= '<a lang="' .$_SITE['wb_meta_' . $_PAGE['lang'.$i]];
-	                        $listItem .= '" href="' .$q . $_PAGE['lang'.$i].'.php';
-	                        if ($_SERVER['QUERY_STRING'] != ""){
-	                        	$listItem .= '?'.$_SERVER['QUERY_STRING'];
-	                        }
-	                        $listItem .= '">' . $_SITE['wb_lang_text_' . $_PAGE['lang'.$i]];
-	                        $listItem .= "</a>";
-	
-	                        $listItem .= '</li>' . "\n";
-	
-	                        $langString = $listItem . $langString;
-	                    }
-	                    echo $langString;
-		            }
-	                ?>
-	 			</ul>
-				</li>
+
+<div class="container">
+	<div class="row">
+		<object id="gcwu-sig" type="image/svg+xml" tabindex="-1" role="img" data="<?php echo $_SIG_IMG_; ?>" aria-label="<?php echo $_SIG_ALT_; ?>"></object>
+		<ul id="gc-bar" class="list-inline">
+			<li><a href="<?php echo $_SITE['gcwu_head_canada_dot_ca_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_canada_dot_ca_'.$_PAGE['lang1']]; ?></a></li>
+			<li><a href="<?php echo $_SITE['gcwu_head_service_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_service_'.$_PAGE['lang1']]; ?></a></li>
+			<li><a href="<?php echo $_SITE['gcwu_head_departments_link_'.$_PAGE['lang1']]; ?>" rel="external"><?php echo $_SITE['gcwu_head_departments_'.$_PAGE['lang1']]; ?></a></li>
+			
+			<li id="wb-lng">
+				<h2><?php echo $_SITE['wb_lang_header_'.$_PAGE['lang1']]; ?></h2>
+			<ul class="list-inline">
+			 <?php
+                if( $_PAGE['nolang'] != "1" ) {
+                    $q= substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], $_PAGE['lang1']."."));
+                    $langString = "";
+                    for($i=2; isset($_PAGE['lang'.$i]); $i++ ) {
+                        $listItem = '<li>';
+                        $listItem .= '<a lang="' .$_SITE['wb_meta_' . $_PAGE['lang'.$i]];
+                        $listItem .= '" href="' .$q . $_PAGE['lang'.$i].'.php';
+                        if ($_SERVER['QUERY_STRING'] != ""){
+                        	$listItem .= '?'.$_SERVER['QUERY_STRING'];
+                        }
+                        $listItem .= '">' . $_SITE['wb_lang_text_' . $_PAGE['lang'.$i]];
+                        $listItem .= "</a>";
+
+                        $listItem .= '</li>' . "\n";
+
+                        $langString = $listItem . $langString;
+                    }
+                    echo $langString;
+	            }
+                ?></ul>
+			</li>
+			
+		</ul>
+		<section class="wb-mb-links col-xs-12 visible-sm visible-xs" id="wb-glb-mn">
+			<h2><?php echo $_SITE['wb_mb_menu_'.$_PAGE['lang1']]; ?></h2>
+			<ul class="pnl-btn list-inline text-right">
+			<li><a href="#mb-pnl" title="<?php echo $_SITE['wb_mb_menu_'.$_PAGE['lang1']]; ?>" aria-controls="mb-pnl" class="overlay-lnk btn btn-sm btn-default" role="button"><span class="glyphicon glyphicon-search"><span class="glyphicon glyphicon-th-list"><span class="wb-inv"><?php echo $_SITE['wb_mb_menu_'.$_PAGE['lang1']]; ?></span></span></span></a></li>
 			</ul>
 			<section class="wb-mb-links col-xs-12 visible-sm visible-xs" id="wb-glb-mn">
 				<h2><?php echo $_SITE['wb_mb_menu_'.$_PAGE['lang1']]; ?></h2>
