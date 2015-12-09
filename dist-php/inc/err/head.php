0e9e2e277a762e2e4c6bc18705f2bf5dc69c18bb
unknown <upsonp@WNNSBIO9019998.ENT.dfo-mpo.ca>
2015-12-09 15:12:25 -0400

    fixed an issue that lead to the GoC flag not being displayed on 404 pages and reverted jQuery to version 2.1.0 because of geomap conflict

--- dist-php/inc/err/head.php
+++ dist-php/inc/err/head.php
@@ -1,6 +1,7 @@
 <?php 
 	/* include theme specific headers if the theme specific /sp-pe/head.php file exists */
-	$_THEME_SP_PE = $_SERVER['DOCUMENT_ROOT'].$_SITE['wb_php_dist_folder'].$_SITE['wb_core_root']."/err/head.php";
+	$_THEME_SP_PE = $_SERVER['DOCUMENT_ROOT'].$_SITE['wb_php_dist_folder']."/".$_SITE['wb_theme']."/err/head.php";
+
 	if( file_exists($_THEME_SP_PE ) ) {
 		include $_THEME_SP_PE;
 	}
